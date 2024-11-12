348500018d07b7954942ad3d3ef08a1f470bd634
Vinicius Manfrin <vinizamara@gmail.com>
2024-11-12 16:26:33 -0300

    Implementação do Aplicativo online

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.89.240.98:5000/api/", // Remover espaço extra
+  baseURL: "https://schedulewell.westus.cloudapp.azure.com:5000/api/", // Remover espaço extra
   headers: {
     Accept: "application/json",
   },
